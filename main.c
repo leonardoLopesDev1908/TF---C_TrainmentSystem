ca2154021b70ffffba8711b75110e7b0feaeb95e
leonardoLopesDev1908 <leonardosilvalls1908@gmail.com>
2026-05-22 17:20:53 -0300

    selecao de questoes

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
+#include "question.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "question.h"
+#include <time.h>
 
 #define MAX_NAME_SIZE 20
 #define SELECTED_QUESTIONS_SIZE 20
 #define MAX_QUESTIONS 400
 
 void load_questions(question_t* q, FILE* file);
-void loop_course();
+void loop_course(question_t* questions);
+void remove_question(question_t* questions, int i);
+void selected_questions(question_t* all_questions, int size_1 
+    question_t* selected_questions, int size_2);
 
 int main()
 {
@@ -23,10 +28,11 @@ int main()
 
     load_questions(questions, fptr);
 
-    //question_t* selected_question = (question_t*) malloc(SELECTED_QUESTIONS_SIZE * sizeof(question_t));
-    //select_questions(&selected_question, &questions);
+    question_t* selected_questions = (question_t*) malloc(SELECTED_QUESTIONS_SIZE * sizeof(question_t));
+    select_questions(&selected_question, SELECTED_QUESTIONS_SIZE,
+            &questions, MAX_QUESTIONS);
 
-    //loop_course();
+    loop_course(selected_questions, SELECTED_QUESTIONS_SIZE);
 
     //testando
     // for(int i = 0; i < MAX_QUESTIONS; i++)
@@ -76,4 +82,48 @@ void load_questions(question_t* q_array, FILE* file)
         q.incorrect_msg[strcspn(q.incorrect_msg, "\n")] = '\0';
         q_array[count++] = q;
     }
+}
+
+void selected_questions(question_t* all_questions, int size_1 
+    question_t* selected_questions, int size_2)
+{
+    srand(time(NULL));
+    for(int i = 0; i < size_1, i++)
+    {
+    
+    }
+}
+
+void loop_course(question_t* questions, size_t count)
+{
+    int i = 0;
+    while(count > 0)
+    {
+        if(i == count - 1) i = 0;
+
+        int user_answer = 0;
+
+        printf("%d: \n%s\n", questions[i].id, questions[i].description);
+        scanf("%d", &user_answer);
+
+        if(user_answer != questions[i].ans)
+        {
+            printf("%s\n", questions[i].incorrect_msg);
+            ++i;
+        }
+        else
+        {
+            --count;
+            printf("%s\n", questions[i].correct_msg);
+            remove_question(questions, i, count);
+        }
+    }
+
+    printf("Todas questoes respondidas\n");
+}
+
+void remove_question(question_t* questions, int index, int size)
+{
+    for(i; i < count - 1; i++)
+        questions[i] = questions[i+1];
 }
